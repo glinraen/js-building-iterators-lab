5ccfc7a2178c6897e7d09ff7c3853a34125fa20c
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-05-09 19:47:30 -0700

    myMap, myReduce and myEach commits

--- myMap.js
+++ myMap.js
@@ -1,18 +1,17 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Map
 
 function myMap(arr, callback) {
-
-//  CODE INSIDE HERE   //
-
+	var output = [];
+	for(var i=0; i<arr.length; i++) {
+		output.push(callback(arr[i], i, arr));
+	}
+	return output;
 }
 
 /*
  Best if you don't code out here.
  If you want to check your code, use `index.js`!
 */
-
-
-
 
 
 
--- myReduce.js
+++ myReduce.js
@@ -1,9 +1,30 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce for more details
 // Don't worry about initialValue at first. You can always add it in later.
 
-function myReduce(arr, callback) {
 
-//  CODE INSIDE HERE   //
+function myReduce(arr, callback, initialValue) {
+	var prevValue;
+	var i = 0;
+
+	if (initialValue == undefined) {
+		prevValue = arr[0]; 
+		i = 1;
+	}
+	else {
+		prevValue = initialValue;
+	}
+	for(; i < arr.length; i++) {
+		//callback(arr[i], i, arr);
+		prevValue = callback(prevValue, arr[i], i, arr);
+
+	}
+
+	// console.log ("look here.", callback());
+	return prevValue;
+
+
+	
+	//var smaller = [];
 
 }
 
